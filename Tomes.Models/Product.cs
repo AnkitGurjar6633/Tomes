091c3cc2b19baac2cb83ef8994bcdc508e9dff81
AnkitGurjar6633 <ankitgurjar6633@gmail.com>
2024-08-17 16:09:15 +0530

    price change

--- Tomes.Models/Product.cs
+++ Tomes.Models/Product.cs
@@ -22,22 +22,22 @@ namespace Tomes.Models
         public string Author { get; set; }
         [Required]
         [Display(Name = "List Price")]
-        [Range(1,1000)]
+        [Range(100,10000)]
         public double ListPrice { get; set; }
 
         [Required]
         [Display(Name = "Price for 1-50")]
-        [Range(1, 1000)]
+        [Range(100, 10000)]
         public double Price { get; set; }
 
         [Required]
         [Display(Name = "Price for 50+")]
-        [Range(1, 1000)]
+        [Range(100, 10000)]
         public double Price50 { get; set; }
 
         [Required]
         [Display(Name = "Price for 100+")]
-        [Range(1, 1000)]
+        [Range(100, 10000)]
         public double Price100 { get; set; }
 
        
